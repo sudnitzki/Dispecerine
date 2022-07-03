b6a157c5284518414ea96aa28a0b6cb5471a1db0
sudnitzki <sudnitzki@gmail.com>
2022-07-03 13:30:52 +0300

    dispatcher_home _index

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dispatcher.apps.py.DispatcherConfig'
 ]
 
 MIDDLEWARE = [
