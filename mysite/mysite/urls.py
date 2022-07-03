0695ff3a70ab966df18c33adec7d864019ba6481
sudnitzki <sudnitzki@gmail.com>
2022-07-03 17:09:19 +0300

    urls changes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from dispatcher.views import index
+from django.urls import include, path
 
 urlpatterns = [
+    path('dispatcher/', include('dispatcher.urls')),
     path('admin/', admin.site.urls),
-    path('dispatcher/', index),
 ]
